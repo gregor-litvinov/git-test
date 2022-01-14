f6827efcb466083b4347aaff5ca4318925dc77ee
USER <user@MacBook-Pro-Alex.local>
2022-01-14 14:06:58 +0200

    first-comt

---
+++ src/components/Test/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>555555</h2>
+        <p>fdgdfgdfgdfgd</p>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: "Test"
+	}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import Test from './components/Test';
 
 Vue.config.productionTip = false
+Vue.component('Test', Test);
 
 new Vue({
   render: h => h(App),
